e3853f020a20f9238b603ca1721821e4c98e2268
lizongqing <lizongqing.sh@superjia.com>
2017-01-03 15:55:02 +0800

    commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-// require('babel-register');
-// var http = require('http');
-// var app = require('./bin/app');
-// var config = require('./config/conf')(process.env['NODE_ENV']);
-// var server = http.createServer(app.callback());
-// console.log('server listen port: ' + config.port);
-// server.listen(config.port);
+require('babel-register');
+var http = require('http');
+var app = require('./bin/app');
+var config = require('./config/conf')(process.env['NODE_ENV']);
+var server = http.createServer(app.callback());
+console.log('server listen port: ' + config.port);
+server.listen(config.port);
 
-var debug = require('debug')('http')
-  , http = require('http')
-  , name = 'My App';
+// var debug = require('debug')('http')
+//   , http = require('http')
+//   , name = 'My App';
 
-// fake app
+// // fake app
 
-debug('booting %s', name);
+// debug('booting %s', name);
 
-http.createServer(function(req, res){
-  debug(req.method + ' ' + req.url);
-  res.end('hello\n');
-}).listen(4444, function(){
-  debug('listening');
-});
+// http.createServer(function(req, res){
+//   debug(req.method + ' ' + req.url);
+//   res.end('hello\n');
+// }).listen(4444, function(){
+//   debug('listening');
+// });
